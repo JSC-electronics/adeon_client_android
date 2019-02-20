48198e022eb504d7063226291d06ed8f14f587d1
Vladimir Zahradnik <vladimir.zahradnik@gmail.com>
2019-02-20 18:48:07 +0100

    Make drawer work

--- app/src/main/java/cz/jsc/electronics/arduinosms/AboutFragment.kt
+++ app/src/main/java/cz/jsc/electronics/arduinosms/AboutFragment.kt
@@ -5,16 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import cz.jsc.electronics.arduinosms.databinding.FragmentDevicesBinding
+import cz.jsc.electronics.arduinosms.databinding.FragmentAboutBinding
 
-class DevicesFragment : Fragment() {
+class AboutFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentDevicesBinding.inflate(inflater, container, false)
+        val binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
