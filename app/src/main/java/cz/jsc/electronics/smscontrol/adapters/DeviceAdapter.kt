9931287b558f8ceaceaabf80197842dcf6acc7b3
Vladimir Zahradnik <vladimir@zahradnik.io>
2019-05-17 18:12:37 +0200

    Capture device icon by camera or load from Gallery

--- app/src/main/java/cz/jsc/electronics/smscontrol/adapters/DeviceAdapter.kt
+++ app/src/main/java/cz/jsc/electronics/smscontrol/adapters/DeviceAdapter.kt
@@ -8,12 +8,12 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import cz.jsc.electronics.smscontrol.DeviceListFragment
 import cz.jsc.electronics.smscontrol.DeviceListFragmentDirections
 import cz.jsc.electronics.smscontrol.R
 import cz.jsc.electronics.smscontrol.data.Device
 import cz.jsc.electronics.smscontrol.databinding.ListItemDeviceBinding
 import cz.jsc.electronics.smscontrol.viewmodels.DeviceListViewModel
-import cz.jsc.electronics.smscontrol.DeviceListFragment
 
 
 /**
@@ -55,6 +55,10 @@ class DeviceAdapter(private val viewModel: DeviceListViewModel) : ListAdapter<De
                     showPopupMenu(it, item)
                 }
                 executePendingBindings()
+
+                item.icon?.let {
+                    deviceIcon.setImageBitmap(it)
+                }
             }
         }
 
--- app/src/main/java/cz/jsc/electronics/smscontrol/data/Converters.kt
+++ app/src/main/java/cz/jsc/electronics/smscontrol/data/Converters.kt
@@ -1,8 +1,13 @@
 package cz.jsc.electronics.smscontrol.data
 
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
+import android.graphics.BitmapFactory
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.io.ByteArrayOutputStream
+
 
 class Converters {
 
@@ -14,5 +19,25 @@ class Converters {
     fun stringToAttributeList(attributeString: String): List<Attribute> {
         val attributeType = object : TypeToken<List<Attribute>>() {}.type
         return Gson().fromJson<List<Attribute>>(attributeString, attributeType)
+    }
+
+    @TypeConverter
+    fun bitmapToByteArray(bitmap: Bitmap?): ByteArray? {
+        bitmap?.let {
+            val stream = ByteArrayOutputStream()
+            bitmap.compress(CompressFormat.PNG, 0, stream)
+            return stream.toByteArray()
+        }
+
+        return null
+    }
+
+    @TypeConverter
+    fun byteArrayToBitmap(bytes: ByteArray?): Bitmap? {
+        bytes?.let {
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        }
+
+        return null
     }
 }
