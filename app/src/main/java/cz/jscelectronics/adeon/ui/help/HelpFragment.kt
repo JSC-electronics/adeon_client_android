6148ec73a4f6f94dd45c5df57e716008a0eb2353
Vladimir Zahradnik <vladimir@zahradnik.io>
2019-06-21 10:31:04 +0200

    Add Feedback functionality

--- app/src/main/java/cz/jscelectronics/adeon/ui/help/HelpFragment.kt
+++ app/src/main/java/cz/jscelectronics/adeon/ui/help/HelpFragment.kt
@@ -1,6 +1,11 @@
 package cz.jscelectronics.adeon.ui.help
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Browser
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -41,6 +46,24 @@ class HelpFragment : Fragment() {
                 val direction =
                     HelpFragmentDirections.actionHelpFragmentToHelpSendCommandsFragment()
                 view.findNavController().navigate(direction)
+            }
+            browseAllArticles.setOnClickListener { view ->
+                val uri = Uri.parse(DOCUMENTATION_ROOT_URI)
+                val context = view.context
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.packageName)
+                try {
+                    context.startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    Log.w("HelpFragment", "Activity was not found for intent, $intent")
+                }
+            }
+            sendFeedback.setOnClickListener {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "plain/text"
+                intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(ADEON_SUPPORT_EMAIL))
+                intent.putExtra(Intent.EXTRA_SUBJECT, ADEON_SUPPORT_EMAIL_SUBJECT)
+                startActivity(Intent.createChooser(intent, ""))
             }
         }
         return binding.root
