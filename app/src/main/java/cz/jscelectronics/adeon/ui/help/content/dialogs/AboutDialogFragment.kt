9cd9999805861670e450b99c29c1bc336c24dfc4
Vladimir Zahradnik <vladimir@zahradnik.io>
2019-07-14 17:45:13 +0200

    Display app version from BuildConfig in About dialog

--- app/src/main/java/cz/jscelectronics/adeon/ui/help/content/dialogs/AboutDialogFragment.kt
+++ app/src/main/java/cz/jscelectronics/adeon/ui/help/content/dialogs/AboutDialogFragment.kt
@@ -1,21 +1,27 @@
 package cz.jscelectronics.adeon.ui.help.content.dialogs
 
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
+import cz.jscelectronics.adeon.BuildConfig
 import cz.jscelectronics.adeon.R
+import cz.jscelectronics.adeon.databinding.DialogAboutBinding
 
-class AboutDialogFragment() : DialogFragment() {
+class AboutDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return context?.let {
-            // Use the Builder class for convenient dialog construction
-            val builder = AlertDialog.Builder(it)
-                .setView(R.layout.dialog_about)
+            activity?.let { activity ->
+                val binding = DialogAboutBinding.inflate(activity.layoutInflater, null, false).apply {
+                    this.version.text = it.getString(R.string.app_version, BuildConfig.VERSION_NAME)
+                }
+
+                // Use the Builder class for convenient dialog construction
+                val builder = AlertDialog.Builder(it)
+                    .setView(binding.alertDialog)
                 builder.create()
+            }
         } ?: throw IllegalStateException("Context cannot be null")
     }
 }
