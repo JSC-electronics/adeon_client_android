cc37a301685bac1171ff7c9ced27f2b3812c9c59
Vladimir Zahradnik <vladimir@zahradnik.io>
2019-08-16 18:26:01 +0200

    Clean up code

--- app/src/main/java/cz/jscelectronics/adeon/ui/device/SendSmsFragment.kt
+++ app/src/main/java/cz/jscelectronics/adeon/ui/device/SendSmsFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
