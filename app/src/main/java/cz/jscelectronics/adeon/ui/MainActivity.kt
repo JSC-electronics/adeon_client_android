fcebe4ecc9bab7d79af35967d2cc51575803eff9
Vladimir Zahradnik <vladimir@zahradnik.io>
2019-08-23 17:53:50 +0200

    Make menu partially work

--- app/src/main/java/cz/jscelectronics/adeon/ui/MainActivity.kt
+++ app/src/main/java/cz/jscelectronics/adeon/ui/MainActivity.kt
@@ -7,14 +7,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import cz.jscelectronics.adeon.R
 import android.content.Intent
+import androidx.navigation.ui.*
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import cz.jscelectronics.adeon.data.PrefManager
 import cz.jscelectronics.adeon.databinding.ActivityMainBinding
+import cz.jscelectronics.adeon.ui.help.content.dialogs.AboutDialogFragment
 import cz.jscelectronics.adeon.ui.intro.IntroActivity
 
 class MainActivity : AppCompatActivity() {
@@ -47,6 +46,34 @@ class MainActivity : AppCompatActivity() {
 
         // Set up navigation menu
         binding.navigationView.setupWithNavController(navController)
+
+        binding.navigationView.setNavigationItemSelectedListener { item ->
+            val handled = when(item.itemId) {
+                R.id.action_import -> {
+                    navController.navigate(R.id.device_list_fragment)
+                    true
+                }
+                R.id.action_export -> {
+                    navController.navigate(R.id.device_list_fragment)
+                    true
+                }
+                R.id.action_delete -> {
+                    navController.navigate(R.id.device_list_fragment)
+                    true
+                }
+                else -> {
+                    // Fallback for all other (normal) cases.
+                    NavigationUI.onNavDestinationSelected(item, navController)
+                }
+            }
+
+            // This is usually done by the default ItemSelectedListener.
+            // But there can only be one! Unfortunately.
+            if (handled) binding.drawerLayout.closeDrawer(binding.navigationView)
+
+            // return whether the action was consumed
+            handled
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
