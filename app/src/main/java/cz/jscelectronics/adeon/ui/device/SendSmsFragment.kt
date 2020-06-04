ee684b3d2519b95b046d79ed15488488b765ebe3
Vladimir Zahradnik <vladimir@zahradnik.io>
2020-06-04 21:37:08 +0200

    Add Ads on Send SMS screen

--- app/src/main/java/cz/jscelectronics/adeon/ui/device/SendSmsFragment.kt
+++ app/src/main/java/cz/jscelectronics/adeon/ui/device/SendSmsFragment.kt
@@ -5,17 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.snackbar.Snackbar
 import cz.jscelectronics.adeon.R
 import cz.jscelectronics.adeon.adapters.AttributesAdapter
 import cz.jscelectronics.adeon.data.Attribute
 import cz.jscelectronics.adeon.databinding.FragmentSendSmsBinding
+import cz.jscelectronics.adeon.ui.billing.viewmodels.BillingViewModel
 import cz.jscelectronics.adeon.ui.device.viewmodels.ManageDeviceViewModel
 import cz.jscelectronics.adeon.utilities.InjectorUtils
 import cz.jscelectronics.adeon.utilities.hideSoftKeyboard
@@ -31,7 +34,9 @@ class SendSmsFragment : Fragment(), AttributesAdapter.AttributeListener {
 
     private var messageText: String? = null
     private lateinit var layout: ConstraintLayout
+    private lateinit var adView: AdView
     private lateinit var manageDeviceViewModel: ManageDeviceViewModel
+    private lateinit var billingViewModel: BillingViewModel
     private val args: SendSmsFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -43,6 +48,7 @@ class SendSmsFragment : Fragment(), AttributesAdapter.AttributeListener {
             InjectorUtils.provideManageDeviceViewModelFactory(requireActivity(), args.deviceId)
         manageDeviceViewModel =
             ViewModelProvider(this, factory).get(ManageDeviceViewModel::class.java)
+        billingViewModel = ViewModelProvider(this).get(BillingViewModel::class.java)
 
         val binding = FragmentSendSmsBinding.inflate(inflater, container, false).apply {
             viewModel = manageDeviceViewModel
@@ -56,6 +62,7 @@ class SendSmsFragment : Fragment(), AttributesAdapter.AttributeListener {
                 manageDeviceViewModel.callDevice(requireActivity(), getPhoneNumber())
             }
         }
+        adView = binding.adView
         layout = binding.sendSmsLayout
 
         val adapter = manageDeviceViewModel.getAttributesAdapter()
@@ -71,6 +78,20 @@ class SendSmsFragment : Fragment(), AttributesAdapter.AttributeListener {
             manageDeviceViewModel.setMessageType(device.messageType, refreshAttributes = false)
             manageDeviceViewModel.initAttributes(device)
         })
+
+        billingViewModel.noAdvertisementsLiveData.observe(this.viewLifecycleOwner, Observer {
+            if (it == null || !it.entitled) {
+                enableAdvertisements()
+            } else {
+                adView.visibility = View.GONE
+            }
+        })
+    }
+
+    private fun enableAdvertisements() {
+        MobileAds.initialize(this.requireContext()) {}
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 
     private fun prepareAndSendSms() {
